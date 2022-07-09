fd50516f640deade52cd169760dfad9dd264f2fc
EmulatorOS <102191619+EmulatorOS@users.noreply.github.com>
2022-07-09 16:54:09 -0500

    css, img, and js

---
+++ js/unityUrls.js
@@ -0,0 +1,9 @@
+window.loadedUrls = true;
+
+function getGameJsonUrl() {
+	return "https://g.hamburgerham.tk/1v1lol/gameLinks/NewCI/1v1/Prod/20/7bdd1837f26d452f9d45d1e94b844039/WebGL.json";
+}
+
+function getGameLoaderUrl() {
+	return "https://g.hamburgerham.tk/1v1lol/gameLinks/NewCI/1v1/Prod/20/7bdd1837f26d452f9d45d1e94b844039/UnityLoader.js";
+}
